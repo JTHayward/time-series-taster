4d6eb9ac89f2001311c16fad27d2dd4fc7bcff87
JTHaywardGDD <72200033+JTHaywardGDD@users.noreply.github.com>
2022-04-12 13:57:42 +0200

    Update for PyCon/PyData

--- answers/rbf.py
+++ answers/rbf.py
@@ -6,12 +6,9 @@ for month in month_peaks.index:
     peak = month_peaks.at[month]
     X_gsf[month] = gsf_feature_maker(peak, days)
 
-X_gsf_train = X_gsf.loc[:'2014']
-X_gsf_test = X_gsf.loc['2015':]
-
 lm_rbf = LinearRegression()
 
-lm_rbf.fit(X_gsf_train, y_train)
+lm_rbf.fit(X_gsf, y)
 
 air_df['linear_rbf'] = lm_rbf.predict(X_gsf)
 
